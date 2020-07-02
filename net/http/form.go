7431e1badce6ed7f080374c6e2bb1edd9de71b91
Dean Karn <Dean.Karn@gmail.com>
2020-07-02 11:55:12 -0700

    Added urlext.TestEncodeToURLValues helper (#5)

--- net/http/form.go
+++ net/http/form.go
@@ -11,7 +11,15 @@ type FormDecoder interface {
 	Decode(interface{}, url.Values) error
 }
 
+// FormEncoder is the type used for encoding form data
+type FormEncoder interface {
+	Encode(interface{}) (url.Values, error)
+}
+
 var (
 	// DefaultFormDecoder of this package, which is configurable
 	DefaultFormDecoder FormDecoder = form.NewDecoder()
+
+	// DefaultFormEncoder of this package, which is configurable
+	DefaultFormEncoder FormEncoder = form.NewEncoder()
 )
