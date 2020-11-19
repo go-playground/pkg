f80596a78176c19997582b29ca9a95bb39bde0b9
Dean Karn <dean@clearbit.co>
2020-11-18 20:53:18 -0800

    update/add mime types

--- net/http/mime_types.go
+++ net/http/mime_types.go
@@ -6,22 +6,23 @@ const (
 
 // Mime Type values for the Content-Type HTTP header
 const (
-	ApplicationJSON       string = "application/json" + charsetUTF8
-	ApplicationJavaScript string = "application/javascript"
-	ApplicationXML        string = "application/xml" + charsetUTF8
-	ApplicationForm       string = "application/x-www-form-urlencoded"
-	ApplicationProtobuf   string = "application/protobuf"
-	ApplicationMsgpack    string = "application/msgpack"
-	ApplicationWasm       string = "application/wasm"
-	ApplicationPDF        string = "application/pdf"
-	TextHTML              string = "text/html" + charsetUTF8
-	TextPlain             string = "text/plain" + charsetUTF8
-	TextMarkdown          string = "text/markdown" + charsetUTF8
-	TextCSS               string = "text/css" + charsetUTF8
-	ImagePNG              string = "image/png"
-	ImageGIF              string = "image/gif"
-	ImageSVG              string = "image/svg+xml"
-	ImageJPEG             string = "image/jpeg"
-	MultipartForm         string = "multipart/form-data"
-	OctetStream           string = "application/octet-stream"
+	ApplicationJSON        string = "application/json" + charsetUTF8
+	ApplicationJavaScript  string = "application/javascript"
+	ApplicationXML         string = "application/xml" + charsetUTF8
+	ApplicationForm        string = "application/x-www-form-urlencoded"
+	ApplicationProtobuf    string = "application/protobuf"
+	ApplicationMsgpack     string = "application/msgpack"
+	ApplicationWasm        string = "application/wasm"
+	ApplicationPDF         string = "application/pdf"
+	ApplicationOctetStream string = "application/octet-stream"
+	TextHTML               string = "text/html" + charsetUTF8
+	TextPlain              string = "text/plain" + charsetUTF8
+	TextMarkdown           string = "text/markdown" + charsetUTF8
+	TextCSS                string = "text/css" + charsetUTF8
+	TextCSV                string = "text/csv"
+	ImagePNG               string = "image/png"
+	ImageGIF               string = "image/gif"
+	ImageSVG               string = "image/svg+xml"
+	ImageJPEG              string = "image/jpeg"
+	MultipartForm          string = "multipart/form-data"
 )
