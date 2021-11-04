d6f4413490de1ab9edee1f8875908a5e40386e52
Dean Karn <Dean.Karn@gmail.com>
2021-11-03 21:06:03 -0700

    Updates & Additions (#9)

--- net/http/mime_types.go
+++ net/http/mime_types.go
@@ -6,23 +6,29 @@ const (
 
 // Mime Type values for the Content-Type HTTP header
 const (
-	ApplicationJSON        string = "application/json" + charsetUTF8
-	ApplicationJavaScript  string = "application/javascript"
-	ApplicationXML         string = "application/xml" + charsetUTF8
-	ApplicationForm        string = "application/x-www-form-urlencoded"
-	ApplicationProtobuf    string = "application/protobuf"
-	ApplicationMsgpack     string = "application/msgpack"
-	ApplicationWasm        string = "application/wasm"
-	ApplicationPDF         string = "application/pdf"
-	ApplicationOctetStream string = "application/octet-stream"
-	TextHTML               string = "text/html" + charsetUTF8
-	TextPlain              string = "text/plain" + charsetUTF8
-	TextMarkdown           string = "text/markdown" + charsetUTF8
-	TextCSS                string = "text/css" + charsetUTF8
-	TextCSV                string = "text/csv"
-	ImagePNG               string = "image/png"
-	ImageGIF               string = "image/gif"
-	ImageSVG               string = "image/svg+xml"
-	ImageJPEG              string = "image/jpeg"
-	MultipartForm          string = "multipart/form-data"
+	ApplicationJSONNoCharset string = "application/json"
+	ApplicationJSON          string = ApplicationJSONNoCharset + charsetUTF8
+	ApplicationJavaScript    string = "application/javascript"
+	ApplicationXMLNoCharset  string = "application/xml"
+	ApplicationXML           string = ApplicationXMLNoCharset + charsetUTF8
+	ApplicationForm          string = "application/x-www-form-urlencoded"
+	ApplicationProtobuf      string = "application/protobuf"
+	ApplicationMsgpack       string = "application/msgpack"
+	ApplicationWasm          string = "application/wasm"
+	ApplicationPDF           string = "application/pdf"
+	ApplicationOctetStream   string = "application/octet-stream"
+	TextHTMLNoCharset               = "text/html"
+	TextHTML                 string = TextHTMLNoCharset + charsetUTF8
+	TextPlainNoCharset              = "text/plain"
+	TextPlain                string = TextPlainNoCharset + charsetUTF8
+	TextMarkdownNoCharset    string = "text/markdown"
+	TextMarkdown             string = TextMarkdownNoCharset + charsetUTF8
+	TextCSSNoCharset         string = "text/css"
+	TextCSS                  string = TextCSSNoCharset + charsetUTF8
+	TextCSV                  string = "text/csv"
+	ImagePNG                 string = "image/png"
+	ImageGIF                 string = "image/gif"
+	ImageSVG                 string = "image/svg+xml"
+	ImageJPEG                string = "image/jpeg"
+	MultipartForm            string = "multipart/form-data"
 )
