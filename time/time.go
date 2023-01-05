1358dbeda61341c1e884f11bbad5a22503c028dd
Dean Karn <Dean.Karn@gmail.com>
2023-01-05 09:28:49 -0800

    Add rfc3339nano go wont correct (#17)
    * Add proper RFC3339Nano to replace the std library bugged implementation

---
+++ time/time.go
@@ -0,0 +1,7 @@
+package timeext
+
+const (
+	// RFC3339Nano is a correct replacement to Go's current time.RFC3339Nano which is NOT sortable and
+	// have no intention of fixing https://github.com/golang/go/issues/19635; this format fixes that.
+	RFC3339Nano = "2006-01-02T15:04:05.000000000Z07:00"
+)
