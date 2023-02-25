286fcdd9a4927c5883827c519efdf401f4e3b275
Dean Karn <dean.karn@gmail.com>
2023-02-25 10:31:57 -0800

    add benchmark

--- time/nanotime_test.go
+++ time/nanotime_test.go
@@ -16,3 +16,15 @@ func TestNanoTime(t *testing.T) {
 		t.Fatalf("nanotime failed to monotonically increase, t1: %d t2: %d", t1, t2)
 	}
 }
+
+func BenchmarkNanoTime(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = nanotime()
+	}
+}
+
+func BenchmarkNanoTimeUsingUnixNano(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = time.Now().UnixNano()
+	}
+}
