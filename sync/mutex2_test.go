c0fd6d9aff002ac980f11d25650509646ed53e0e
Dean Karn <dean.karn@gmail.com>
2023-03-06 19:02:09 -0800

    make it safer

--- sync/mutex2_test.go
+++ sync/mutex2_test.go
@@ -5,6 +5,7 @@ package syncext
 
 import (
 	resultext "github.com/go-playground/pkg/v5/values/result"
+	"sync"
 	"testing"
 
 	. "github.com/go-playground/assert/v2"
@@ -12,51 +13,57 @@ import (
 
 func TestMutex2(t *testing.T) {
 	m := NewMutex2(make(map[string]int))
-	m.Lock()["foo"] = 1
-	m.Unlock()
+	guard := m.Lock()
+	guard.T["foo"] = 1
+	guard.Unlock()
 
 	m.PerformMut(func(m map[string]int) {
 		m["boo"] = 1
 	})
-	myMap := m.Lock()
+	guard = m.Lock()
+	myMap := guard.T
 	Equal(t, 2, len(myMap))
 	Equal(t, myMap["foo"], 1)
 	Equal(t, myMap["boo"], 1)
-	Equal(t, m.TryLock(), resultext.Err[map[string]int](struct{}{}))
-	m.Unlock()
+	Equal(t, m.TryLock(), resultext.Err[MutexGuard[map[string]int, *sync.Mutex]](struct{}{}))
+	guard.Unlock()
 
 	result := m.TryLock()
 	Equal(t, result.IsOk(), true)
-	m.Unlock()
+	result.Unwrap().Unlock()
 }
 
 func TestRWMutex2(t *testing.T) {
 	m := NewRWMutex2(make(map[string]int))
-	m.Lock()["foo"] = 1
+	guard := m.Lock()
+	guard.T["foo"] = 1
 	Equal(t, m.TryLock().IsOk(), false)
 	Equal(t, m.TryRLock().IsOk(), false)
-	m.Unlock()
+	guard.Unlock()
 
 	m.PerformMut(func(m map[string]int) {
 		m["boo"] = 2
 	})
-	mp := m.Lock()
+	guard = m.Lock()
+	mp := guard.T
 	Equal(t, mp["foo"], 1)
 	Equal(t, mp["boo"], 2)
-	m.Unlock()
+	guard.Unlock()
 
-	myMap := m.RLock()
+	rguard := m.RLock()
+	myMap := rguard.T
 	Equal(t, len(myMap), 2)
 	Equal(t, m.TryRLock().IsOk(), true)
-	m.RUnlock()
+	rguard.RUnlock()
 
 	m.Perform(func(m map[string]int) {
 		Equal(t, 1, m["foo"])
 		Equal(t, 2, m["boo"])
 	})
-	myMap = m.RLock()
+	rguard = m.RLock()
+	myMap = rguard.T
 	Equal(t, len(myMap), 2)
 	Equal(t, myMap["foo"], 1)
 	Equal(t, myMap["boo"], 2)
-	m.RUnlock()
+	rguard.RUnlock()
 }
