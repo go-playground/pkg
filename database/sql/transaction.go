1d1a6e8dfa6c94d91c454e57f62e0bb7a028253d
Dean Karn <dean.karn@gmail.com>
2023-05-08 17:08:23 -0700

    change DoTransaction function signature

--- database/sql/transaction.go
+++ database/sql/transaction.go
@@ -10,12 +10,12 @@ import (
 )
 
 // DoTransaction is a helper function that abstracts some complexities of dealing with a transaction and rolling it back.
-func DoTransaction[T any](ctx context.Context, opts *sql.TxOptions, conn *sql.DB, fn func(*sql.Tx) resultext.Result[T, error]) resultext.Result[T, error] {
+func DoTransaction[T any](ctx context.Context, opts *sql.TxOptions, conn *sql.DB, fn func(context.Context, *sql.Tx) resultext.Result[T, error]) resultext.Result[T, error] {
 	tx, err := conn.BeginTx(ctx, opts)
 	if err != nil {
 		return resultext.Err[T, error](err)
 	}
-	result := fn(tx)
+	result := fn(ctx, tx)
 	if result.IsErr() {
 		_ = tx.Rollback()
 		return result
