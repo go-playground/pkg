5d7fdeb5f7050a3eb22b0aaf79b8d372e74ed079
Dean Karn <dean.karn@gmail.com>
2023-06-30 22:44:17 -0700

    cleanup

--- time/instant.go
+++ time/instant.go
@@ -8,24 +8,22 @@ import "time"
 // Instant represents a monotonic instant in time.
 //
 // Instants are opaque types that can only be compared with one another and allows measuring of duration.
-type Instant struct {
-	monotonic int64
-}
+type Instant int64
 
 // NewInstant returns a new Instant.
 func NewInstant() Instant {
-	return Instant{monotonic: NanoTime()}
+	return Instant(NanoTime())
 }
 
 // Elapsed returns the duration since the instant was created.
 func (i Instant) Elapsed() time.Duration {
-	return time.Duration(NanoTime() - i.monotonic)
+	return time.Duration(Instant(NanoTime()) - i)
 }
 
 // Since returns the duration elapsed from another Instant, or zero is that Instant is later than this one.
 func (i Instant) Since(instant Instant) time.Duration {
-	if instant.monotonic > i.monotonic {
+	if instant > i {
 		return 0
 	}
-	return time.Duration(i.monotonic - instant.monotonic)
+	return time.Duration(i - instant)
 }
