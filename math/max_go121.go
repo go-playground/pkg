de2d9ffd7acbc50627de284b24bb91b40d1ff7dc
Dean Karn <deankarn@reaver1.local>
2024-02-12 20:41:56 -0800

    version test

--- math/max_go121.go
+++ math/max_go121.go
@@ -1,26 +1,17 @@
-//go:build go1.18 && !go1.21
-// +build go1.18,!go1.21
+//go:build go1.21
+// +build go1.21
 
 package mathext
 
 import (
-	"math"
-
 	constraintsext "github.com/go-playground/pkg/v5/constraints"
 )
 
 // Max returns the larger value.
 //
 // NOTE: this function does not check for difference in floats of 0/zero vs -0/negative zero using Signbit.
+//
+// // Deprecated: use the new std library `max` instead.
 func Max[N constraintsext.Number](x, y N) N {
-	// special case for floats
-	// IEEE 754 says that only NaNs satisfy f != f.
-	if x != x || y != y {
-		return N(math.NaN())
-	}
-
-	if x > y {
-		return x
-	}
-	return y
+	return max(x, y)
 }
