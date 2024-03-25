07683f0ae28756f404495c8212fb0b5891677abd
Dean Karn <dean.karn@gmail.com>
2024-03-25 12:59:09 -0700

    update example with fallible logic

--- _examples/net/http/retrier/main.go
+++ _examples/net/http/retrier/main.go
@@ -22,7 +22,13 @@ func main() {
 	type Test struct {
 		Date time.Time
 	}
+	var count int
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if count < 2 {
+			count++
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
 		_ = httpext.JSON(w, http.StatusOK, Test{Date: time.Now().UTC()})
 	}))
 	defer server.Close()
@@ -44,7 +50,7 @@ func main() {
 	fmt.Printf("Response: %+v\n", result)
 
 	// `Retrier` configuration is copy and so the base `Retrier` can be used and even customized for one-off requests.
-	// eg for this request we only want max attempt of 2 instead of default.
+	// eg for this request we change the max attempts from the default configuration.
 	err = retrier.MaxAttempts(errorsext.MaxAttempts, 2).Do(ctx, fn, &result, http.StatusOK)
 	if err != nil {
 		panic(err)
